2aa04c4391c2ddae30fa1cab925e6ae4112d3005
christian.benitez <christian.p.benitez@gmail.com>
2022-05-22 14:10:04 -0700

    Add first route.

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const port = process.env.PORT || 5000
 
 const app = express()
 
-const port = process.env.PORT || 3000
+app.use('/api/goals', require('./routes/goalRoutes'))
 
 app.listen(port, () => {
     console.log('Hello World')
