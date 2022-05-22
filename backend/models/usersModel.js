3ccd0eecb7e17cb107b0edd22a9543e418d62aa1
christian.benitez <christian.p.benitez@gmail.com>
2022-05-22 15:32:13 -0700

    Initialize User model. Associate goal with a user.

---
+++ backend/models/usersModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please add a name']
+    }, 
+    email: {
+        type: String,
+        required: [true, 'Please add a email'],
+        unique: true
+    }, 
+    username: {
+        type: String,
+        required: [true, 'Please add a useranme']
+    }, 
+    password: {
+        type: String,
+        required: [true, 'Please add a password']
+    }, 
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model('User', goalSuserSchemachema)
