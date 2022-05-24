0111bef7f7c132de6c077fdf6f10652ba1a6ec6f
christian.benitez <christian.p.benitez@gmail.com>
2022-05-23 18:18:14 -0700

    Prepare for Heroku

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const dotenv = require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
@@ -16,6 +17,19 @@ app.use('/api/goals', require('./routes/goalRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
 
 app.use(errorHandler)
+
+// Serve frontend
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html'))
+    })
+} else {
+    app.get('/', (req, res) => {
+        res.send('Please set to production')
+    })
+}
 
 app.listen(port, () => {
     console.log('Hello World')
